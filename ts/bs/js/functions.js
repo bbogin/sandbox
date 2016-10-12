0d6244d9dbdcf055c0dfb4d60935afb42c123b48
Ben Bogin <benbogin@gmail.com>
2016-10-11 19:29:15 -0700

    more

--- ts/bs/js/functions.js
+++ ts/bs/js/functions.js
@@ -1,1 +1,44 @@
-console.log("hello world")
+// Select Plan Javascript
+
+// jQuery(document).ready(function(){
+//   $('.block').click(function() {
+//   	if (!$(this).hasClass("info")) {
+//       $(this).next('.info').slideToggle('slow');
+//       return false;
+//     }
+//   });
+// });
+
+
+$(document).ready(function(){
+	$( ".block-col" ).accordion({ active: false, collapsible: true });
+	$(".comparethis").click(function () {
+		$(this).parent().toggleClass("selected")
+		$(this).parent().prev().toggleClass("selected")
+		$(".block-col").accordion({active: false}).click();
+
+	})
+
+
+$('#close').click(function () {
+    $("#accordion").accordion({active: false}).click();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
